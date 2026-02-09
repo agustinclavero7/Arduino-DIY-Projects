a0c7e471802d642f1b88277326daf543945e8518
AgusCla <agustin.clavero@gmail.com>
2026-02-09 15:48:29 -0300

    Added potentiometer and pause function. Most stable version

--- 02.SpontaneousBreath_O_Matic/02.SpontaneousBreath_O_Matic.ino
+++ 02.SpontaneousBreath_O_Matic/02.SpontaneousBreath_O_Matic.ino
@@ -2,24 +2,24 @@
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
-#define SMOTOR2_PIN         6
-#define SERVO_FW           20
-#define SERVO_STEP          1
-#define SERVO_STEP_DELAY    3
+#define SMOTOR2_PIN 6
+#define SERVO_RESTORE_CONS 15
+#define SERVO_STEP 1
+#define SERVO_STEP_DELAY 2
 
-#define LCD_RS  53
-#define LCD_E   51
-#define LCD_D4  49
-#define LCD_D5  47
-#define LCD_D6  45
-#define LCD_D7  43
+#define LCD_RS 13
+#define LCD_E  12
+#define LCD_D4 11
+#define LCD_D5 10
+#define LCD_D6 9
+#define LCD_D7 8
 
-#define LED_PIN   41
+#define LED_PIN 7
 
-#define ENCODER_A_PIN   3
-#define ENCODER_B_PIN   4
-#define ENCODER_SW_PIN  5
+#define ADJUST_PIN  A0
+#define OK_BUTTON   4
 #define CALIBRATION_PIN 2
+#define POTE_FILTER 10
 
 #define NUMBER_OF_SAMPLES 5
 
@@ -37,10 +37,9 @@ int initialPos;
 int endPos;
 
 //Adjust settings
-volatile int actualEncoderPosition = 12;
-volatile int lastEncoderPosition = actualEncoderPosition;
-volatile unsigned long lastInterruptTime = millis();
-volatile int encoderDebounceDelay = 5;
+int prevPoteValue;
+int actualPoteValue;
+bool bpmChangeFlag = false;
 
 //LCD
 double monitorBpmArray[NUMBER_OF_SAMPLES] ={12.0,12.0,12.0,
@@ -56,22 +55,21 @@ int ledState = LOW;
 volatile bool isCalibration = false;
 
 //Pause
-byte pauseDelay = 150;
-unsigned long lastTimeSw = millis();
-volatile bool pauseStateFlag = false;
+unsigned long pauseTimeStart = millis();
+int pauseDelay = 150;
+bool pauseFlag = false;
 
 void setup(){
 	Serial.begin(115200);
   lcd.begin(16,2);
-  pinMode(ENCODER_A_PIN,INPUT);
-  pinMode(ENCODER_B_PIN,INPUT);
-  pinMode(ENCODER_SW_PIN,INPUT_PULLUP);
+  pinMode(OK_BUTTON,INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(CALIBRATION_PIN, INPUT);
+  actualPoteValue = analogRead(ADJUST_PIN);
+  prevPoteValue = actualPoteValue;
   sMotor2.attach(SMOTOR2_PIN, 1000, 2000);
   attachInterrupt(digitalPinToInterrupt(CALIBRATION_PIN),
                             calibrationInterrupt,RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A_PIN),
-                                   encoderInterrupt,LOW);
   //Welcome message
   lcd.print("  Spontaneous   ");
   lcd.setCursor(0,1);
@@ -80,38 +78,53 @@ void setup(){
   lcd.clear();
   //Restore Settings
   initialPos = EEPROM.read(EEPROM_CAL_ADRESS);
-  endPos = initialPos + SERVO_FW;
+  endPos = initialPos + SERVO_RESTORE_CONS;
 }
 
 void loop(){
   unsigned long timeNow = millis();
   //Main function → move the servos Or enter calibration screen
   if(isCalibration){
-    calibrationRoutine(actualEncoderPosition);
+    calibrationRoutine();
+    servoTimeStart = timeNow - servoDelay;
+    prevPoteValue = actualPoteValue;
   }
-  else if(pauseStateFlag){
-    pauseFunction();
-    Serial.print("Pausa");
+  else if (pauseFlag){
+    //Serial.println("Pause mode");
+    pauseState();
+    actualPoteValue = analogRead(ADJUST_PIN);
+    int howMuchPoteMove = abs(prevPoteValue - actualPoteValue);
+    if(howMuchPoteMove > POTE_FILTER){
+      pauseFlag = false;
+      lcd.clear();
+    }
   }
   else{
     //Move servos and show data.
+    //Serial.println("Main mode");
     if(timeNow - servoTimeStart > servoDelay){
       int servoTimePeriod = timeNow - servoTimeStart;
+      servoMove();
       calculateBpm(servoTimePeriod);
       showBpmOnLcd();
-      servoMove();
       servoTimeStart += servoDelay;
     }
-    //Change Bpm w/ encoder
-    if(actualEncoderPosition != lastEncoderPosition){
-      changeBreathRate(actualEncoderPosition);
+    //Change Bpm w/ potentiometer
+    actualPoteValue = analogRead(ADJUST_PIN);
+    int howMuchPoteMove = abs(prevPoteValue - actualPoteValue);
+    //Serial.println(howMuchPoteMove);
+    if(howMuchPoteMove > POTE_FILTER){
+      bpmChangeFlag = true;
     }
-    else if(timeNow - lastTimeSw > pauseDelay){
-      if(digitalRead(ENCODER_SW_PIN) == LOW){
-        lastTimeSw += pauseDelay;
-        pauseStateFlag = true;
+    else if (digitalRead(OK_BUTTON) == LOW && bpmChangeFlag == false){
+      if(timeNow - pauseTimeStart > pauseDelay){
+        pauseTimeStart+= pauseDelay;
+        pauseFlag = true;
+        lcd.clear();
       }
     }
+    changeBreathRate(actualPoteValue);
+  
     //Blink Led
     if(timeNow - ledTimerStart > ledDelay){
       ledTimerStart += ledDelay;
---
+++ 02.SpontaneousBreath_O_Matic/CalibrationFunction.ino
@@ -0,0 +1,25 @@
+void calibrationInterrupt(){
+  if(isCalibration == false)
+    isCalibration = true;
+}
+
+void calibrationRoutine(){
+  int adjustPote = analogRead(ADJUST_PIN);
+  int motorPosition = adjustPote / 5.7;
+  lcd.setCursor(0,0);
+  lcd.print("Cal Screen             ");
+  lcd.setCursor(0,1);
+  lcd.print("Motor Pos: ");
+  lcd.print(motorPosition);
+  lcd.print("           ");
+  //sMotor1.write(motorPosition);
+  sMotor2.write(motorPosition);
+  if(digitalRead(OK_BUTTON) == LOW)
+  {
+    initialPos = motorPosition;
+    EEPROM.write(EEPROM_CAL_ADRESS,initialPos);
+    endPos = initialPos + SERVO_RESTORE_CONS;
+    isCalibration = false;
+    lcd.clear();
+  } 
+}
